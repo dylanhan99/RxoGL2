86530475d041205a93efca121764f97022dc8c1d
dylanhan99 <dylanhan99@gmail.com>
2021-03-20 12:58:06 +0800

    Changed to GL_BGRA

--- RxoGL2-Core/Singletons/TextureCache.cpp
+++ RxoGL2-Core/Singletons/TextureCache.cpp
@@ -24,7 +24,7 @@ void TextureData::Load()
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
 	glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
-	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_Width, m_Height, 0, GL_RGBA, GL_UNSIGNED_BYTE, m_LocalBuffer);
+	glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA8, m_Width, m_Height, 0, GL_BGRA, GL_UNSIGNED_BYTE, m_LocalBuffer);
 	glGenerateMipmap(GL_TEXTURE_2D);
 	Unbind();
 }
